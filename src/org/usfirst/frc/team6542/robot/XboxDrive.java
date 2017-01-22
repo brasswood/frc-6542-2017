7f7fa7a847195aadaa086710001c384d084635fc
Andrew Riachi <andrew.riachi@gmail.com>
2017-01-22 13:46:31 -0600

    non-working snapshot

--- src/org/usfirst/frc/team6542/robot/XboxDrive.java
+++ src/org/usfirst/frc/team6542/robot/XboxDrive.java
@@ -6,19 +6,38 @@ import edu.wpi.first.wpilibj.XboxController;
 //like RobotDrive
 public class XboxDrive {
 	
-	SpeedController left;
-	SpeedController right;
+	SpeedController frontLeft;
+	SpeedController frontRight;
+	SpeedController backLeft;
+	SpeedController backRight;
 	XboxController controller;
-	
+	/**
+	 * Constructs XboxDrive given two SpeedControllers and an XboxController
+	 * @param left			The controller you want to be the left side of the robot
+	 * @param right			The controller you want to be the right side of the robot
+	 * @param controller	The XboxController object you would like to use
+	 */
 	public XboxDrive(SpeedController left, SpeedController right, XboxController controller) {
-		this.left = left;
-		this.right = right;
+		frontLeft = left;
+		frontRight = right;
 		this.controller = controller;
 	}
 	
 	// Just In Case (TM)
-	public XboxDrive(SpeedController one, SpeedController two, SpeedController three, SpeedController four, XboxController controller) {
-		
+	/**
+	 * Constructs XboxDrive given four SpeedControllers (starting from front right and going counterclockwise) and an XboxController
+	 * @param frontRight	The controller for the front right side of the robot
+	 * @param frontLeft 	The controller for the front left side of the robot
+	 * @param backLeft		The controller for the back left side of the robot
+	 * @param backRight		The controller for the back right side of the robot
+	 * @param controller	The XboxController object you would like to use
+	 */
+	public XboxDrive(SpeedController frontRight, SpeedController frontLeft, SpeedController backLeft, SpeedController backRight, XboxController controller) {
+		// merge controllers on same side
+		this.frontRight = frontRight;
+		this.frontLeft = frontLeft;
+		this.backLeft = backLeft;
+		this.backRight = backRight;
 	}
 	
 	//called every TeleopPeriod in IterativeRobot?
@@ -27,21 +46,26 @@ public class XboxDrive {
 		// getTriggerAxis returns between 0 and 1.
 		double speed = controller.getTriggerAxis(GenericHID.Hand.kRight) - controller.getTriggerAxis(GenericHID.Hand.kLeft);
 		double x = controller.getX(GenericHID.Hand.kLeft);
-		double[] sides = getEachSide(x);
 		// whatever rTrigger is, scale
 		// left and right side proportional to that
-		left.set(sides[0] * speed);
-		right.set(sides[1] * speed);
+		
 		
 		
 
 	}
-	// left comma right
-	private double[] getEachSide(double steeringFactor) {
-		double[] sides = new double[5];
-		sides[0] = 1 - steeringFactor;
-		sides[1] = steeringFactor;
-		return sides;
+	
+	protected void setLeftRightMotors(double leftOutput, double rightOutput) {
+		// if only two sides were specified in constructor, fronts were used
+		frontLeft.set(leftOutput);
+		if (backLeft != null) {
+			backLeft.set(leftOutput);
+		}
+		
+		frontRight.set(rightOutput);
+		if (backRight != null) {
+				backRight.set(rightOutput);
+		}
 	}
+
 	
 }
